051c1222fda26fe0a135c3d75439527123278b0b
Chethan Krishna <chethan.cmk@gmail.com>
2023-07-13 17:15:14 +0000

    Iteration 1

---
+++ dummy_commits.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get the source and destination directories
+source_dir="helm-guestbook-"
+yaml_file_name = 
+
+# Loop 5 times
+for i in {1..2}; do
+  # Clone the directory    
+  sed -i "s,0.1,0.2,g" ${source_dir}$i"/values.yaml"
+  git add .
+  git commit -m "Iteration ${i}"
+  git push origin
+  # Print a success message
+  echo "Directory updated successfully (iteration $i)."
+  sleep 10
+done
