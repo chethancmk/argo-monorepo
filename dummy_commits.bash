16a19e0dd915f315f1b6ddb17008901941822573
Chethan Krishna <chethan.cmk@gmail.com>
2023-07-14 06:29:13 +0000

    Iteration 1

--- dummy_commits.bash
+++ dummy_commits.bash
@@ -5,10 +5,10 @@ source_dir="helm-guestbook-"
 yaml_file_name = 
 
 # Loop 5 times
-for i in {1..50}; do
+for i in {1..5}; do
   # Clone the directory    
-  sed -i "s,0.2,0.1,g" ${source_dir}$i"/values.yaml"
-  sed -i "s,replicaCount: 2,replicaCount: 1,g" ${source_dir}$i"/values.yaml"
+  sed -i "s,0.1,0.2,g" ${source_dir}$i"/values.yaml"
+  sed -i "s,replicaCount: 1,replicaCount: 2,g" ${source_dir}$i"/values.yaml"
   
 #   sed -i "s,IfNotPresent,Always,g" ${source_dir}$i"/values.yaml"
   
